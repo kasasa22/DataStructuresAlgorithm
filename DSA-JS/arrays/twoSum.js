03a759f19a2cbcbd0329d817e1ca0dae24f5d024
kasasa22 <kasasatrevor25@gmail.com>
2024-10-13 22:42:06 +0300

    arrays

---
+++ DSA-JS/arrays/twoSum.js
@@ -0,0 +1,17 @@
+const twoSum = (arr, target)=>{
+    for (i=0;i<arr.length;i++){
+        for(j=i+1;j<arr.length;j++){
+            if (arr[i]+arr[j]===target){
+                console.log(arr[i],arr[j])
+                return[i,j]
+
+            }
+           
+        }
+    } 
+    return []
+}
+
+answer = twoSum([1,3,5,6], 8)
+console.log(answer)
+
